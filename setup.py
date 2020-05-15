53014556a635c1f62bb1ab1e59b6e7efcaa87958
fanchengyan <fanchy14@lzu.edu.cn>
2020-05-15 22:11:07 +0800

    init

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="data-downloader-fanchegyan", # Replace with your own username
+    version="0.0.1",
+    author="fanchegyan",
+    author_email="fanchy14@lzu.edu.cn",
+    description="Data downloader",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Fanchengyan/data-downloader",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
