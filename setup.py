f9cb4ae87939d26f185519a6154223ee14e6b9ae
fanchengyan <fanchy14@lzu.edu.cn>
2024-03-20 15:58:27 +0800

    change version to v1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="UTF-8") as fh:
 
 setuptools.setup(
     name="data_downloader",
-    version="0.1.0",
+    version="1.0",
     author="fanchegyan",
     author_email="fanchy14@lzu.edu.cn",
     description="Make downloading scientific data much easier",
