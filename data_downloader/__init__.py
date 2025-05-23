25a041f59995b36ab79312bfc0e6f46f8b8e66e6
fanchengyan <fanchy14@lzu.edu.cn>
2025-05-23 10:47:07 +0800

    restructure with logging module

--- data_downloader/__init__.py
+++ data_downloader/__init__.py
@@ -1,1 +1,8 @@
 from . import downloader, parse_urls, services, utils
+import logging
+
+logger = logging.getLogger("data_downloader")
+logger.setLevel(logging.DEBUG)
+logger.propagate = True  # 确保日志向上传播
+logger.addHandler(logging.NullHandler())
+
