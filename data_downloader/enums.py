8fc79c44e429f3eb3fab65c8b584814ce52eb0ee
fanchengyan <fanchy14@lzu.edu.cn>
2025-07-11 21:13:45 +0800

    add StrEnum

---
+++ data_downloader/enums.py
@@ -0,0 +1,53 @@
+from __future__ import annotations
+
+from enum import Enum, auto
+
+from typing_extensions import Self
+
+from data_downloader.logging import setup_logger
+
+logger = setup_logger(__name__)
+
+
+class StrEnum(str, Enum):
+    """A string enum class.
+
+    This class is used to support StrEnum before Python 3.11.
+    """
+
+    _value_: str
+
+    def __new__(cls, value: str | auto, *args, **kwargs) -> Self:
+        if not isinstance(value, (str, auto)):
+            msg = f"Values of StrEnums must be strings: {value!r} is a {type(value)}"
+            logger.error(msg)
+            raise TypeError(msg)
+        return super().__new__(cls, value, *args, **kwargs)
+
+    def __str__(self) -> str:
+        return str(self.value)
+
+    @staticmethod
+    def _generate_next_value_(name: str, *args, **kwargs) -> str:
+        return name
+
+
+class BaseConstants(StrEnum):
+    @classmethod
+    def variables(cls) -> list[str]:
+        """all available variables"""
+        return list(cls.__members__.keys())
+
+
+class JobType(BaseConstants):
+    AUTORIFT = auto()
+    RTC_GAMMA = auto()
+    INSAR_GAMMA = auto()
+    INSAR_ISCE_BURST = auto()
+
+
+class JobStatus(BaseConstants):
+    SUCCEEDED = auto()
+    FAILED = auto()
+    RUNNING = auto()
+    PENDING = auto()
